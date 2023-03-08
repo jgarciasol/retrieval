c44c1d2ccad92957799e517cf94bf4c07ed6a764
jgarciasol <99057885+jgarciasol@users.noreply.github.com>
2023-03-08 02:32:37 -0500

    made changes to how to remove tokens that occur once in corpus

--- phase2/tokenizer_p2.py
+++ phase2/tokenizer_p2.py
@@ -16,7 +16,7 @@ def get_filenames(input_dir):
     return filenames
 
 
-def parse(input_dir, output_dir, stop_words_list):
+def parse(input_dir, output_dir, stop_words_set):
     files = get_filenames(input_dir)
     files_length = len(files)
     #used to store token frequency and avoids checks for existence of a key in a dict
@@ -35,9 +35,7 @@ def parse(input_dir, output_dir, stop_words_list):
             #The two lines below are how I stripped tokens in the Phase 1 of the project
             #stripped = re.sub(r'[^\sa-zA-Z ]', '\n', text)
             #tokens = [token.lower() for token in stripped.split() if len(token) > 1 and token.lower() not in stop_words_list]
-            tokens = [token for token in stripped if token not in stop_words_list and len(token) > 1]
-
-           
+            tokens = [token for token in stripped if token not in stop_words_set and len(token) > 1]
             for token in tokens:
                 if token not in token_freq:
                     #create empty inner dict if token has not been encountered
@@ -45,12 +43,17 @@ def parse(input_dir, output_dir, stop_words_list):
                 if file not in token_freq[token]:
                     token_freq[token][file] = 0  #default value is 0
                 token_freq[token][file] += 1
-
+            
             #used a set to not count duplicates as it resulted in negative TF-IDF
             #keeps track of num of documents contain a certain token
             for token in set(tokens):
                 document_freq[token]+=1
-    
+
+            #METHOD 1 of removing tokens that appear once in corpus
+            # appear_once = [token for token in token_freq if document_freq[token] == 1]
+            # for token in appear_once:
+            #     del token_freq[token]
+
         output_file = os.path.join(output_dir, os.path.basename(file))
         #removes the .html extension, replaces it with .wts
         output_file = os.path.splitext(output_file)[0] + '.wts'
@@ -59,6 +62,10 @@ def parse(input_dir, output_dir, stop_words_list):
         with open(output_file, 'w') as out:
             for token in token_freq.keys():
                 if file in token_freq[token]:
+                    #METHOD 2 of ignore tokens that only appear once in corpus
+                    #Not sure if this is the correct way to not count tokens that occur once in entire corpus
+                    if document_freq[token] == 1:
+                        continue
                     tf = token_freq[token][file] / len(tokens)
                     idf = math.log(files_length / document_freq[token])
                     tf_idf = tf*idf
@@ -66,10 +73,11 @@ def parse(input_dir, output_dir, stop_words_list):
 
         if i % 100 == 0:
             print(f'Number of files processed: {i + 1} ')
+
     end = time.time()
     time_taken = (end - start) * 1000 #convert to milliseconds
     print(f'Time taken is ~ {time_taken} milliseconds')
-    plt.plot([0, len(files)], [0, time_taken])
+    plt.plot([0, i], [0, time_taken])
     plt.xlabel('Number of Files')
     plt.ylabel('Time taken(ms)')
     plt.show()
@@ -84,7 +92,7 @@ if __name__ == "__main__":
 
     #reads in stop words
     with open('stopwords.txt', 'r') as f:
-        stop_words_list = {line.strip() for line in f}
-
-    #print(len(stop_words_list))
-    parse(input_dir, output_dir, stop_words_list)
+        stop_words_set = {line.strip() for line in f}
+    #print(type(stop_words_set))
+    #print(len(stop_words_set))
+    parse(input_dir, output_dir, stop_words_set)
