7e07a6d4116e16ebfe7f2826c1e9b8e4125cc86d
jgarciasol <99057885+jgarciasol@users.noreply.github.com>
2023-04-07 20:41:12 -0400

    added other files that are produced from script

--- phase3/index.py
+++ phase3/index.py
@@ -78,7 +78,7 @@ def inverted_index(token_freq, doc_freq, output_dir):
         for token, docs in token_freq.items():
             idf = math.log(num_docs / len(docs))
             #writes token, num of docs containing token, and pos
-            d_file.write(f'{token}\n{len(docs)}\n{posting_pos}\n\n')
+            d_file.write(f'token: {token}\nnumber of docs: {len(docs)}\nposting position: {posting_pos}\n')
 
             for file, weight in docs.items():
                 tf = weight
